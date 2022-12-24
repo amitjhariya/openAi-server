42238a9b63a2129208e867ed0ac6ddc6faff8d71
amit jhariya <amit.jhariya93@gmail.com>
2022-12-24 21:09:53 +0530

    Open Repo

--- controllers/openAi.js
+++ controllers/openAi.js
@@ -3,8 +3,7 @@ import { OpenAIApi } from 'openai'
 import { openAiConfig } from '../config/index.js'
 const openAi = new OpenAIApi(openAiConfig);
 
-export const imageGenerate = async (req, res) => {
-    
+export const imageGenerate = async (req, res) => {    
     const { search, size } = req.body
     const imageSize = size === 'medium' ? '512x512': size === 'large' ? '1024x1024':'256x256'
     try {
